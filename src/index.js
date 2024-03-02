64e91db11a1fe75cac1db02507030e6249e378c2
_ziyiyap <yapziyi13@gmail.com>
2024-03-02 12:43:07 +0800

    token

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const {Client, IntentsBitField} = require('discord.js');
 
 const client = new Client({
@@ -5,3 +6,9 @@ const client = new Client({
         IntentsBitField.Flags.Guilds
     ]
 })
+
+client.on('ready', () => {
+    console.log(`You have power over your mind — not outside events. Realize this, and you will find strength.`);
+  });
+
+  client.login(process.env.TOKEN);
