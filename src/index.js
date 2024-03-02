fca230530c0dad49fda4d271812ff69227a5441a
_ziyiyap <yapziyi13@gmail.com>
2024-03-02 12:47:32 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ const {Client, IntentsBitField} = require('discord.js');
 
 const client = new Client({
     intents: [
-        IntentsBitField.Flags.Guilds
-    ]
-})
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ],
+});
 
 client.on('ready', () => {
     console.log(`You have power over your mind — not outside events. Realize this, and you will find strength.`);
